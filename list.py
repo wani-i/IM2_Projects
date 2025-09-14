50a55650b68e6a123d77a4d1916746222e9a9f46
wani-i <dwanderturf@gmail.com>
2025-09-14 18:39:18 +0800

    Homework In IM2

---
+++ list.py
@@ -0,0 +1,31 @@
+# === Using List ===
+
+r = int(input("Enter row: "))
+c = int(input("Enter col: "))
+
+arr = []
+
+for i in range(r):
+    print(f"Row {i+1}")
+    row = []
+    for j in range(c):
+        n = float(input(f"Enter number{j+1}: "))
+        row.append(n)
+    arr.append(row)
+
+x = float(input("\nSearch: "))
+
+print("\nThe numbers are:")
+for row in arr:
+    print(" ".join(map(str, row)))
+
+pos = []
+for i in range(r):
+    for j in range(c):
+        if arr[i][j] == x:
+            pos.append(f"Row {i}, Col {arr[i][j]}")
+
+if pos:
+    print(f"\nNumber {x} found at " + " and ".join(pos))
+else:
+    print(f"\nNumber {x} not found.")
